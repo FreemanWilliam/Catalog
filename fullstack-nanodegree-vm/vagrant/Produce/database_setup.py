10e68fb127561f910efdcd2e4cf4961a21e02ff8
FreemanWilliam <myhousemyrules69@yahoo.com>
2019-06-29 15:57:59 -0400

    Combined lines three and four in the file database_setup.py.

--- fullstack-nanodegree-vm/vagrant/Produce/database_setup.py
+++ fullstack-nanodegree-vm/vagrant/Produce/database_setup.py
@@ -1,7 +1,6 @@
 import os
 import sys
-from sqlalchemy import
-Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String
 
 from sqlalchemy.ext.declarative import
 declarative_base
