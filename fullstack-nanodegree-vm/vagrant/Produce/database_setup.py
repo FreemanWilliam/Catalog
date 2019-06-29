01eeeb8a521c17ab3dfd8d6f5bdf2c615c852ad0
FreemanWilliam <myhousemyrules69@yahoo.com>
2019-06-29 15:34:37 -0400

    Defined the column price to be of type String in the class ProduceItem in the file database_setup.py.

--- fullstack-nanodegree-vm/vagrant/Produce/database_setup.py
+++ fullstack-nanodegree-vm/vagrant/Produce/database_setup.py
@@ -20,7 +20,7 @@ class ProduceItem(Base):
 	id = Column(Integer, primary_key = True)
 	name = Column(String(80), nullable = False)
 	description = Column(String(250))
-	price = Column()
+	price = Column(String(8))
 
 
 
