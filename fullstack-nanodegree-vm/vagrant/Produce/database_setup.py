18c63b91b816a566bd58dda7c5b82c453f2fe6b0
FreemanWilliam <myhousemyrules69@yahoo.com>
2019-06-29 16:00:28 -0400

    Deleted the empty line six in the file database_setup.py.

--- fullstack-nanodegree-vm/vagrant/Produce/database_setup.py
+++ fullstack-nanodegree-vm/vagrant/Produce/database_setup.py
@@ -3,7 +3,6 @@ import sys
 from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
-
 from sqlalchemy import create_engine
 
 Base = declarative_base()
